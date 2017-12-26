f37a6b452837cfa67c7c08a70601a72968d49b60
Robert Basic <robertbasic.com@gmail.com>
2017-12-26 14:29:13 +0100

    Redirect if it's a POST request
    Need to use the correct 302 status code

--- handler/home.go
+++ handler/home.go
@@ -14,6 +14,11 @@ var tpl = template.Must(template.ParseFiles(tpls...))
 type Home struct {}
 
 func (h *Home) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+
 	tpl.Execute(w, nil)
 }
 
