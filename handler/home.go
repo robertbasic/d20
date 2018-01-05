b1465251b8bd7189fef48c1e4464203133079ef7
Robert Basic <robertbasic.com@gmail.com>
2018-01-05 18:11:35 +0100

    Read the cookie on the home page

--- handler/home.go
+++ handler/home.go
@@ -1,8 +1,13 @@
 package handler
 
 import (
-	"net/http"
+	"encoding/base64"
+	"encoding/json"
 	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/robertbasic/d20/roll"
 )
 
 var tpls = []string{
@@ -11,9 +16,34 @@ var tpls = []string{
 }
 var tpl = template.Must(template.ParseFiles(tpls...))
 
-type Home struct {}
+type Home struct{}
 
 func (hh *Home) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	tpl.Execute(w, nil)
+	var data []roll.Dice
+	defer tpl.Execute(w, data)
+
+	c, err := r.Cookie("dice")
+	if err != nil {
+		return
+	}
+
+	data, err = getDice(c.Value)
+	if err != nil {
+		log.Println(err)
+	}
 }
 
+func getDice(c string) ([]roll.Dice, error) {
+	data, err := base64.StdEncoding.DecodeString(c)
+	if err != nil {
+		return nil, err
+	}
+
+	var dice []roll.Dice
+	err = json.Unmarshal(data, &dice)
+	if err != nil {
+		return nil, err
+	}
+
+	return dice, nil
+}
